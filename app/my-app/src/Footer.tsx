7b2e37788dc17def9676a6d02a4c0db675304eb9
DanilKoshnarev <146003208+DanilKoshnarev@users.noreply.github.com>
2024-11-30 20:10:58 +0200

    Create Footer.tsx

---
+++ app/my-app/src/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer>
+      <ul>
+        <li><a href="#privacy">Политика конфиденциальности</a></li>
+        <li><a href="#terms">Условия использования</a></li>
+        <li><a href="#contact">Контакты</a></li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
