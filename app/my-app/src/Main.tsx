313d3a429f60d19ddbc173505090921803d51d0d
DanilKoshnarev <146003208+DanilKoshnarev@users.noreply.github.com>
2024-11-30 20:10:19 +0200

    Create Main.tsx

---
+++ app/my-app/src/Main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PostList from './PostList';
+
+const Main: React.FC = () => {
+  return (
+    <main>
+      <PostList />
+    </main>
+  );
+};
+
+export default Main;
