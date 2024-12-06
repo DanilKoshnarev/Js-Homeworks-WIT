f91fcb435f233754c8f80d0a446e7e90501b3c28
DanilKoshnarev <146003208+DanilKoshnarev@users.noreply.github.com>
2024-12-06 17:47:59 +0200

    Update App.tsx

--- app/my-app/src/App.tsx
+++ app/my-app/src/App.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
+import CategoryFilter from './CategoryFilter';
 
 const App: React.FC = () => {
+  const [selectedCategory, setSelectedCategory] = useState('Все');
+  const categories = ['Все', 'маркетинг', 'программирование', 'котики', 'фильмы'];
+
   return (
     <div>
       <Header />
-      <Main />
+      <CategoryFilter
+        categories={categories}
+        selectedCategory={selectedCategory}
+        onSelectCategory={setSelectedCategory}
+      />
+      <Main category={selectedCategory} />
       <Footer />
     </div>
   );
