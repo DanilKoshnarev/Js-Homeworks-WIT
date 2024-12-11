94351246d0965f7b403ee426d77533766fe8a939
DanilKoshnarev <146003208+DanilKoshnarev@users.noreply.github.com>
2024-12-11 19:13:59 +0200

    Create PostPage.tsx

---
+++ app/my-app/src/PostPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PostPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Post Page</h1>
+      <p>Happi Happi Happi </p>
+    </div>
+  );
+};
+
+export default PostPage;
