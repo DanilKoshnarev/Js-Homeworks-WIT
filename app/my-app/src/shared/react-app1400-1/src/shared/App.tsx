2897a0260212a597b4a4718beb923d89fd3a989a
DanilKoshnarev <146003208+DanilKoshnarev@users.noreply.github.com>
2025-01-31 22:12:27 +0200

    Update App.tsx

--- app/my-app/src/shared/react-app1400-1/src/shared/App.tsx
+++ app/my-app/src/shared/react-app1400-1/src/shared/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ProductListPage } from "../pages/ProductListPage/ProductListPage";
 import { ProductPage } from "../pages/ProductPage/ProductPage";
 import { MainPage } from "../pages/MainPage/MainPage";
+import { LikedPostsPage } from "../pages/LikedPostsPage/LikedPostsPage";
 import { FavoritesProvider } from "./FavoritesContext";
 
 export function App() {
@@ -15,6 +16,7 @@ export function App() {
                             <Route path="/" element={<MainPage />} />
                             <Route path="/products" element={<ProductListPage />} />
                             <Route path="/product/:id" element={<ProductPage />} />
+                            <Route path="/liked-posts" element={<LikedPostsPage />} />
                         </Route>
                     </Routes>
                 </BrowserRouter>
