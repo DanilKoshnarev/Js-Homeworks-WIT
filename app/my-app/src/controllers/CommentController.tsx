e125f4b1d1eb584ec79a693173fb0307ca1d6ff3
DanilKoshnarev <146003208+DanilKoshnarev@users.noreply.github.com>
2025-02-15 17:56:47 +0200

    Update CommentController.tsx

--- app/my-app/src/controllers/CommentController.tsx
+++ app/my-app/src/controllers/CommentController.tsx
@@ -1,14 +1,14 @@
-import { fetchCommentsByPostId, fetchCommentsByUserId } from '../services/CommentService';
-import { Comment } from '../models/Comment';
+import { fetchCommentsByPostId, fetchCommentsByUserId } from '../services/GenericCommentService';
+import { Comment } from '../models/GenericComment';
 
 export const getCommentsByPostId = (req, res) => {
   const { postId } = req.body;
-  const comments: Comment[] = fetchCommentsByPostId(postId);
+  const comments: Comment<any>[] = fetchCommentsByPostId(postId);
   res.json(comments);
 };
 
 export const getCommentsByUserId = (req, res) => {
   const { userId } = req.body;
-  const comments: Comment[] = fetchCommentsByUserId(userId);
+  const comments: Comment<any>[] = fetchCommentsByUserId(userId);
   res.json(comments);
 };
